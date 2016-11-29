4fb99ab8f484f3f93a65df73556cd3f938cce4b4
isaactwong <isaactwong@gmail.com>
2016-11-28 23:11:18 -0500

    Completed the problem for 22 Nov 2016.

---
+++ 22NOV2016_missing_items.hs
@@ -0,0 +1,9 @@
+import Data.List
+
+-- Given two lists, find all the items in the first list that are not present in the second list. For instance, if (5 15 2 20 30 40 8 1) is the first list and (2 20 15 30 1 40 0 8) is the second list, the item 5 is present in the first list but not in the second list.
+
+notIn :: (Eq a) => [a] -> [a] -> [a]
+notIn xs ys = filter (flip notElem $ ys) xs
+
+-- Cheap and easy with Set differencing from Haskell.
+-- notIn xs ys = xs \\ ys
