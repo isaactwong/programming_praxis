170820fab9f10d3cd5cdf81fffe4881eff39eefe
isaactwong <isaactwong@gmail.com>
2016-12-03 23:21:57 -0500

    Solved unwrapping a spiral.

---
+++ 01JUN2010_unwrapping_sprials.hs
@@ -0,0 +1,21 @@
+import Data.List
+
+-- The task is to enumerate the elements of a matrix in spiral order. For instance, consider the matrix:
+
+--  1  2  3  4
+--  5  6  7  8
+--  9 10 11 12
+-- 13 14 15 16
+-- 17 18 19 20
+
+-- The spiral starts across the first row, yielding 1, 2, 3, and 4. Then the spiral turns right and runs down the right column, yielding 8, 12, 16, and 20. The spiral turns right again and runs across the bottom row, from right to left, yielding 19, 18, and 17. Then up the first column with 13, 9, 5, right with 6 and 7, down with 11 and 15, right to 14, and up to 10. Thus, unwrapping the given matrix in a spiral gives the list of elements 1, 2, 3, 4, 8, 12, 16, 20, 19, 18, 17, 13, 9, 5, 6, 7, 11, 15, 14, and 10.
+
+-- Your task is to write a function to unwrap spirals. 
+
+
+-- Always take the first row as is, then rotate the matrix counterclockwise (reverse . transpose OR transpose . reverse) and then recurse.
+unspiral :: [[a]] -> [a]
+unspiral [] = []
+unspiral (x : xs) = x ++ unspiral (reverse . transpose $ xs)
+
+
