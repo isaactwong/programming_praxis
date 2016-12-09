11d174f7c8f356d4e83f46daeb28faa325d0ce09
isaactwong <isaactwong@gmail.com>
2016-12-08 22:21:44 -0500

    Renamed missing items and added date.

--- missing_items.hs
+++ missing_items.hs
@@ -1,5 +1,6 @@
 import Data.List
 
+-- 22Nov2016
 -- Given two lists, find all the items in the first list that are not present in the second list. For instance, if (5 15 2 20 30 40 8 1) is the first list and (2 20 15 30 1 40 0 8) is the second list, the item 5 is present in the first list but not in the second list.
 
 notIn :: (Eq a) => [a] -> [a] -> [a]
