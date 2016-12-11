59fd92569994bd77df7249da591bfb3ffc3c5961
isaactwong <isaactwong@gmail.com>
2016-12-11 00:24:29 -0500

    Complered career cup problem.

--- career_cup.hs
+++ career_cup.hs
@@ -1,3 +1,16 @@
+import Data.List
+
 -- 6 Dec 2016
 -- Given two sorted arrays, efficiently find the median of the combined array.
-median :: (Ord a) => [a] -> [a] -> a
+median :: (Floating a, Ord a) => [a] -> [a] -> a
+median rs ls = med $ rs ++ ls
+
+
+med :: (Floating a, Ord a) => [a] -> a
+med xs
+    | even l       = head (drop (l `div` 2) sxs)
+    | otherwise    = avg (take 2 (drop (l `div` 2) sxs))
+  where
+        l   = (length xs) - 1
+        sxs = sort xs
+        avg as = ((as !! 0) + (as !! 1))/2
