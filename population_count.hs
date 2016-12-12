9219b7377cf4c611e169003afdc5765191a86350
isaactwong <isaactwong@gmail.com>
2016-12-11 23:19:30 -0500

    Created file for population count problem

---
+++ population_count.hs
@@ -0,0 +1,5 @@
+-- 28Jan2011
+-- The population count of a bitstring is the number of set bits (1-bits) in the string. For instance, the population count of the number 23, which is represented in binary as 10111, is 4. The population count is used in cryptography and error-correcting codes, among other topics in computer science; some people use it as an interview question. The population count is also known as Hamming weight.
+
+-- Your task is to write a function that determines the population count of a number representing a bitstring
+
