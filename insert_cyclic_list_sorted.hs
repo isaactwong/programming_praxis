77e09a55eea1c545496ec16e01860931bf2b2b2f
isaactwong <isaactwong@gmail.com>
2017-01-05 23:24:00 -0500

    Completed basic functions for the crazy infinite list!

--- insert_cyclic_list_sorted.hs
+++ insert_cyclic_list_sorted.hs
@@ -8,14 +8,24 @@ Given a cyclic list with elements in sorted order, write a function to insert a 
 
 -- Circular, doubly-linked list based on the standard list type.
 -- from https://wiki.haskell.org/Tying_the_Knot#Other_Examples
-data DList a = DLNode (DList a) a (DList a)
-mkDList :: (Show a) => [a] -> DList a
+-- This is an amazing use of lazy evaluation!
+data DList a = DLNode (DList a) a (DList a) deriving (Show)
+
+mkDList :: [a] -> DList a
 mkDList [] = error "must have at least one element"
 mkDList xs = let (first,last) = go last xs first
              in first
 
-go :: (Show a) => DList a -> [a] -> DList a -> (DList a, DList a)
+go :: DList a -> [a] -> DList a -> (DList a, DList a)
 go prev [] next = (next, prev)
 go prev (x:xs) next = let this        = DLNode prev x rest
                           (rest,last) = go this xs next
                       in (this,last)
+
+takeF :: Integer -> DList a -> [a]
+takeF 0 _ = []
+takeF n (DLNode _ x next) = x : (takeF (n-1) next)
+
+takeR :: Integer -> DList a -> [a]
+takeR 0 _ = []
+takeR n (DLNode previous x _) = x : (takeR (n-1) previous)
